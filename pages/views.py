f4d0a12053fe46c07eb7449440e25c0d176757ab
Tikumporn Wankvar <tikumporn@Macintosh.local>
2017-10-20 01:40:36 +0700

    Adding bootstrap messages with Mixin

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,4 @@
+from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import render
 from django.views.generic import View, FormView, CreateView
 from newsletter.forms import JoinForm
@@ -7,7 +8,11 @@ from newsletter.forms import JoinForm
 #     def get(self, request, *args, **kwargs):
 #         return render(request, 'pages/home.html')
 
-class HomeView(CreateView):
+class HomeView(SuccessMessageMixin,CreateView):
     template_name = 'pages/home.html'
     form_class = JoinForm
     success_url = '/'
+
+    def get_success_message(self, cleaned_data):
+        print (cleaned_data)
+        return "Thank your for joining !"
