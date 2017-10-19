8cf4ce2bed21d29aab24ab052b936037c06243e1
Tikumporn Wankvar <tikumporn@Macintosh.local>
2017-10-20 02:18:13 +0700

    Add Page and dynamic home page

--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,7 @@ from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import render
 from django.views.generic import View, FormView, CreateView
 from newsletter.forms import JoinForm
+from .models import Page
 # Create your views here.
 
 # class HomeView(View):
@@ -12,6 +13,11 @@ class HomeView(SuccessMessageMixin,CreateView):
     template_name = 'pages/home.html'
     form_class = JoinForm
     success_url = '/'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        context['page_obj'] = Page.objects.all().first()
+        return context
 
     def get_success_message(self, cleaned_data):
         print (cleaned_data)
