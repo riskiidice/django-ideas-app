2b139c56905f989137497a89164c38d9e7648a0e
Tikumporn Wankvar <tikumporn@Macintosh.local>
2017-10-19 23:21:20 +0700

    static-live

--- ideas/settings.py
+++ ideas/settings.py
@@ -129,12 +129,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root")
-
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-#STATIC_ROOT = "/home/cfedeploy/webapps/cfehome_static_root/"
+STATIC_ROOT = os.path.join(BASE_DIR, "live-static", "static-root")
 
 MEDIA_URL = "/media/"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "live-static-files", "media-root")
+MEDIA_ROOT = os.path.join(BASE_DIR, "live-static", "media-root")
